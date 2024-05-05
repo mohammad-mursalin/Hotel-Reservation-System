7a38e13acdab3184160876c39beb0018f74eca58
mohammad_mursalin <mursalinlamon@gmail.com>
2024-05-05 16:03:11 +0600

    database informations

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 
 public class Main {
+
+    private static String url = "jdbc:postgresql://localhost/postgresdemo";
+    private static String username = "postgres";
+    private static String password = ""
     public static void main(String[] args) {
 
 
